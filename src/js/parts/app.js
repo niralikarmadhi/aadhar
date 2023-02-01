94b424c2538f1e773be4d8681bd877f44ecd2226
niralikarmadhi <118166254+niralikarmadhi@users.noreply.github.com>
2023-02-01 16:43:41 +0530

    responsive

--- src/js/parts/app.js
+++ src/js/parts/app.js
@@ -1,70 +1,131 @@
 import 'slick-carousel';
 export class App {
-    init() {
+  init() {
 
-        $('.team-slider').slick({
-            dots: false,
-            arrows: true,
-            infinite: false,
-            autoplay: false,
-            slidesToShow: 8,
-            // fade: true,
-            cssEase: 'linear',
-            prevArrow: $(".slider--prev"),
-            nextArrow: $(".slider--next"),
-          });
+    $('.team-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      autoplay: false,
+      slidesToShow: 8,
+      // fade: true,
+      cssEase: 'linear',
+      prevArrow: $(".slider--prev"),
+      nextArrow: $(".slider--next"),
 
-          $('.lady-slider').slick({
-            dots: false,
-            arrows: false,
-            infinite: true,
-            autoplay: true,
-            slidesToShow: 1,
-            fade: true,
-            speed: 500,
-            cssEase: 'linear',
-          });
-
-          $('.testimonial-slider').slick({
-            dots: false,
-            arrows: true,
-            infinite: true,
-            speed: 500,
-            autoplay: true,
-            slidesToShow: 1,
-            fade: true,
-            cssEase: 'linear',
-            prevArrow: $(".slider--prev3"),
-            nextArrow: $(".slider--next3"),
-          });
-
-          $('.main-slider').slick({
-            dots: false,
-            arrows: true,
-            infinite: true,
-            speed: 500,
-            autoplay: true,
-            slidesToShow: 1,
-            fade: true,
-            cssEase: 'linear',
-            prevArrow: $(".slider--prev3"),
-            nextArrow: $(".slider--next3"),
-          });
-          $('.logo-slider').slick({
-            dots: false,
-            arrows: false,
-            infinite: true,
-            autoplay: true,
-            speed: 500,
+      responsive: [
+        {
+          breakpoint: 1700,
+          settings: {
             slidesToShow: 7,
-            cssEase: 'linear',
-          });
-    
-    }
-    
+          }
+        },
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 6,
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 5,
+          }
+        },{
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4,
+          }
+        },{
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+          }
+        },{
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+          }
+        },{
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1,
+          },
+        }
+      ],
+    });
 
-    slickSLider (){
+    $('.lady-slider').slick({
+      dots: false,
+      arrows: false,
+      infinite: true,
+      autoplay: true,
+      slidesToShow: 1,
+      fade: true,
+      speed: 500,
+      cssEase: 'linear',
+    });
 
-    }
+    $('.testimonial-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 1,
+      fade: true,
+      cssEase: 'linear',
+      prevArrow: $(".slider--prev3"),
+      nextArrow: $(".slider--next3"),
+    });
+
+    $('.main-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+      slidesToShow: 1,
+      fade: true,
+      cssEase: 'linear',
+      prevArrow: $(".slider--prev3"),
+      nextArrow: $(".slider--next3"),
+    });
+    $('.logo-slider').slick({
+      dots: false,
+      arrows: false,
+      infinite: true,
+      autoplay: true,
+      speed: 500,
+      slidesToShow: 7,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 6,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 4,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 3,
+          },
+        }
+      ],
+    });
+
+  }
+
+
+  slickSLider() {
+
+  }
 
 }
